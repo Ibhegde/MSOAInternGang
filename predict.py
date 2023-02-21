45478ee8397b76876201b7acc974d246698614c2
Hegde, Bharathkumar Shripad <bharathkumar.shripad.hegde@intel.com>
2023-02-21 12:45:16 +0000

    update

--- predict.py
+++ predict.py
@@ -1,14 +1,30 @@
 import pandas as pd
-
+import os
 import argparse
 
-parser = argparse.ArgumentParser(prog="predict.py", description="Prediction python module for Hackathon")
-parser.add_argument("--path", action="store", default="/data/", required=True, type=str, help="TEST data folder path")
-parser.add_argument(--metadata, action="store", default="TEST_images_metadata.csv", required=True, type=str, help="Test images metadata file path")
+parser = argparse.ArgumentParser(
+    prog="predict.py", description="Prediction python module for Hackathon"
+)
+parser.add_argument(
+    "--path",
+    action="store",
+    default="/data/",
+    required=True,
+    type=str,
+    help="TEST data folder path",
+)
+parser.add_argument(
+    "--metadata",
+    action="store",
+    default="TEST_images_metadata.csv",
+    required=True,
+    type=str,
+    help="Test images metadata file path",
+)
 
 args = parser.parse_args()
 
-#all images are available here.
+# all images are available here.
 all_images = os.listdir(args.path)
 
 # Do your magic here....
@@ -16,7 +32,7 @@ all_images = os.listdir(args.path)
 
 # Get predictions at ref_id level.
 
-# Create dataframe with the below specified template/ the sample submission file format mentioned on the Platform. 
+# Create dataframe with the below specified template/ the sample submission file format mentioned on the Platform.
 # There are 3 tasks, if you're not solving any task, please exclude corresponding column while generating below dataframe.
 
 # ref_id,       POA_attribution, activity_category, activity_type
@@ -28,11 +44,10 @@ all_images = os.listdir(args.path)
 # ----------------------------------------------------------------
 # DON'T CHANGE THE BELOW FILE NAME, ELSE, INFERENCE PROCESS FAILS.
 # ----------------------------------------------------------------
-# save the dataframe as ===== 'predictions.csv' ===== 
+# save the dataframe as ===== 'predictions.csv' =====
 # df.to_csv("predictions.csv", index=False)
 
 # Exit.
-
 
 
 #### To TEST Locally ####
